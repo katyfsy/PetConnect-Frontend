06b729e7dc77b0962dd3756de0e6d84eadc576c4
Vincent Le <levin@oregonstate.edu>
2022-08-29 15:33:57 -0700

    added react-bootstrap

--- pet-connect-frontend/src/Pages/Home.js
+++ pet-connect-frontend/src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigationbar from '../Components/Navbar'
 
 
 function Home() {
   return(
     <div>
       <h1>hello from home</h1>
+      < Navigationbar/>
     </div>
   )
 }
