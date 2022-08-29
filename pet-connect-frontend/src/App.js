a2d7d97ab06699d30cba03081b35d8394b611efd
Lucas <lucasbonner@protonmail.com>
2022-08-29 14:43:42 -0500

    adds react-router

--- pet-connect-frontend/src/App.js
+++ pet-connect-frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Search from './Components/Search';
+import Home from './Pages/Home';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div>petConnect</div>
-      <Search/>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
 
     </div>
   );
