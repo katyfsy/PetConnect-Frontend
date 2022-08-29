61619a0ab41f5e25058cec56f285d7ea9e835611
Marilene Soares da Costa <lene626@gmail.com>
2022-08-29 14:48:17 -0600

    adds minimum CSS to search results

--- pet-connect-frontend/src/Components/SearchResults.js
+++ pet-connect-frontend/src/Components/SearchResults.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
   Link
 } from "react-router-dom";
-
+import './SearchResults.css';
 
 // bar: [Number of matches] search word  distance (arrow)  near <location>
 
@@ -20,7 +19,17 @@ function SearchResults(){
             <Link to="/">Home</Link>
         </Router>
         <div>
-            <p> list of pets </p>
+            <div className="topBar">
+                <p> Top bar [# matches] search-word  distance (arrow)  near LOCATION </p>
+            </div>
+            <div className="main">
+                <div className="advancedSearchCol">
+                    <p> column left: advanced search (dropdowns)</p>
+                </div>
+                <div className="searchResultsCol">
+                    <p> column right: cards (mini profiles) - for now just a list of names </p>
+                </div>
+            </div>
         </div>
      </div>
     )
