28d9c05b25aa8567760dbfb6012ae068664ced36
katyfsy <katyfsy@gmail.com>
2022-08-30 15:04:38 -0400

    search default dropdown with axios get connected

--- pet-connect-frontend/src/Components/Search/DefaultSearchDropdown.js
+++ pet-connect-frontend/src/Components/Search/DefaultSearchDropdown.js
@@ -1,10 +1,34 @@
 import React, {useState, useEffect} from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../../node_modules/bootstrap/dist/js/bootstrap.min';
+
 
 function DefaultSearchDropdown() {
   return(
-    <div id="defaultSearchDropdown">
-      <li>All Cats</li>
-      <li>All Dogs</li>
+    <div>
+      {/* <!-- Button trigger modal --> */}
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      {/* <!-- Modal --> */}
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="bts btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
