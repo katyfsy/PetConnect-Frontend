578da7de20ef4fd8dc56a8a3c371947e7560879f
Benis Tambe <will.jenifer@yahoo.com>
2022-09-02 12:37:57 -0400

    adding form

--- app/src/Components/PetProfile/AddAPetForm.js
+++ app/src/Components/PetProfile/AddAPetForm.js
@@ -3,5 +3,27 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 class AddAPetForm extends Component {
+
+  handleOnChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <>
+        <Form onSubmit={this.handleOnSubmit}>
+        </Form>
+
+        <br />
+      </>
+    );
+  }
 }
 export default AddAPetForm;
