d541e23ee5eaf8026b5c4d88e28462f137f7c7df
Benis Tambe <will.jenifer@yahoo.com>
2022-09-02 12:36:50 -0400

    added add a pet form component

---
+++ app/src/Components/PetProfile/AddAPetForm.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+class AddAPetForm extends Component {
+}
+export default AddAPetForm;
