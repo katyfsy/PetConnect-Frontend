17ff3d00e18c375e099b89c86bb945d4b0548356
Yu Zhang <rita890927@gmail.com>
2022-09-07 13:01:10 -0400

    add messages link to navbar

--- app/src/Components/Default/Navbar.js
+++ app/src/Components/Default/Navbar.js
@@ -5,29 +5,31 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigationbar() {
-    return (
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/profile">Profile</Nav.Link>
-                <Nav.Link href="/pets">Pets</Nav.Link>
-                <NavDropdown className="justify-content-end" title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      );
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/profile">Profile</Nav.Link>
+            <Nav.Link href="/pets">Pets</Nav.Link>
+            <Nav.Link href="/messages">Messages</Nav.Link>
+
+            <NavDropdown className="justify-content-end" title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigationbar;
