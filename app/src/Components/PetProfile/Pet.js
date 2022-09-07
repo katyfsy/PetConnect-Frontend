1ad8960d660a25b5818d799733272254762a6118
Benis Tambe <will.jenifer@yahoo.com>
2022-09-07 17:38:02 -0400

    Coommiting to pulll

--- app/src/Components/PetProfile/Pet.js
+++ app/src/Components/PetProfile/Pet.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useParams } from "react-router-dom";
 
 import Navigationbar from '../Default/Navbar';
 import Header from '../Default/Header';
@@ -6,7 +7,25 @@ import Header from '../Default/Header';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-function Pet({petId, requiredPetFields}) {
+function Pet() {
+
+  const [thisPet, setThisPet] = useState(null);
+  let petId = useParams();
+  console.log(petId)
+
+  useEffect(() => {
+    fetch(`http://a920770adff35431fabb492dfb7a6d1c-1427688145.us-west-2.elb.amazonaws.com:8080/api/pets/${petId}`)
+    .then((res) => res.json())
+    // .catch((err) => {
+    //   console.error(err);
+    // })
+    .then((data) => {
+      console.log(data);
+      setThisPet(data);
+    })
+
+  })
+
   return (
     <>
     <Container>
@@ -15,8 +34,8 @@ function Pet({petId, requiredPetFields}) {
     <Navigationbar/>
     <Container></Container>
     <br />
-    
-    <div>pet goes here { /*requiredPetFields.owner*/ }</div>
+
+    <div>pet goes here {thisPet}</div>
     <br  />
     </>
       
