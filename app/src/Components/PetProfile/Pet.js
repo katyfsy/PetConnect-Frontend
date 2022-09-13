f3606776ec86533f48994f95f3c9455edb93d091
Benis Tambe <will.jenifer@yahoo.com>
2022-09-13 15:03:13 -0400

    styling pet profile

--- app/src/Components/PetProfile/Pet.js
+++ app/src/Components/PetProfile/Pet.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import EditPet from "./EditPet";
-import Navigationbar from "../Default/Navbar";
-import Header from "../Default/Header";
-
-import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function Pet() {
   const [thisPet, setThisPet] = useState({});
+  const [isEdit, setIsEdit] = useState(false);
   let petId = useParams();
-  // console.log(petId);
-
+  console.log(petId);
+  let isLoggedIn = localStorage.getItem("username") == null ? false : true;
+  let user = localStorage.getItem("username");
   useEffect(() => {
     fetch(
       `http://a920770adff35431fabb492dfb7a6d1c-1427688145.us-west-2.elb.amazonaws.com:8080/api/pets/${petId.id}`
@@ -24,37 +26,88 @@ function Pet() {
         console.log(data);
         setThisPet(data);
       });
-
   }, []);
-  // console.log("Pet Idddddd");
-  // console.log(petId.id);
+
+  function handleOnDelete() {
+    fetch(`http://localhost:8080/api/pets/${petId.id}`, { method: "DELETE" })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.error(err);
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  if (thisPet == null) {
+    return null;
+  }
   return (
     <>
-      <Container>
-        <Header />
-      </Container>
-      <Navigationbar />
-      <Container>
-        <br />
-        
+      {user == thisPet.owner ? (
+        <>
+          <button onClick={() => setIsEdit(!isEdit)}>Edit</button>
+          <button onClick={handleOnDelete}>Delete</button>
+        </>
+      ) : null}
+      <br />
+
+      <Row className="mb-3">
+        <Col>
+          <Image
+            src={thisPet.coverPhoto}
+            roundedCircle
+            className="profile-photo"
+          />
+          <div></div>
+        </Col>
+        <Col>
+          <Row className="mb-3" id="user-name">
+            <h1 className="pet-name">{thisPet.name}</h1>
+            <p className="pet-location">{thisPet.location}</p>
+          </Row>
+          <Row className="mb-3" id="contact-pet-owner">
+            <Button variant="primary" size="lg" href="/messages">
+              Contact My Owner
+            </Button>
+            <p className="user-email">{form.email}</p>
+          </Row>
+          <Row className="mb-3" id="user-description">
+            <h3 className="user-description">Description:</h3>
+          </Row>
+          <Row className="mb-3">
+            <p className="text-start">{form.description}</p>
+          </Row>
+        </Col>
         <div>Pet Information Goes Here </div>
-        <p>Name: {thisPet.name}</p>
-        <p>Owner: {thisPet.owner}</p>
-        <p>Location: {thisPet.location}</p>
-        <p>Type: {thisPet.type}</p>
-        <p>Description: {thisPet.description}</p>
+        {!isEdit ? (
+          <>
+            <p>Name: {thisPet.name}</p>
+            <p>Owner: {thisPet.owner}</p>
+            <p>Location: {thisPet.location}</p>
+            <p>Type: {thisPet.type}</p>
+            <p>Description: {thisPet.description}</p>
 
-        <p>Weight: {thisPet.weight}</p>
-        <p>Age: {thisPet.age}</p>
-        <p>Reproductive Status: {thisPet.reproductiveStatus}</p>
-      
-        <p>Likes: {thisPet.favoriteCount}</p>
-        <p>Reported?: {thisPet.reported}</p>
-        <p>Adopted?: {thisPet.adopted}</p>
-        <p>Name: {thisPet.name}</p>
+            <p>Weight: {thisPet.weight}</p>
+            <p>Age: {thisPet.age}</p>
+            <p>
+              Reproductive Status:{" "}
+              {thisPet.reproductiveStatus ? "No Kids" : "Yes Kids"}
+            </p>
 
-        <br />
-      </Container>
+            <p>Likes: {thisPet.favoriteCount}</p>
+            <p>Reported: {thisPet.reported ? "true" : "false"}</p>
+            <p>Adopted: {thisPet.adopted ? "true" : "false"}</p>
+          </>
+        ) : (
+          <EditPet
+            thisPet={thisPet}
+            setIsEdit={setIsEdit}
+            setThisPet={setThisPet}
+          />
+        )}
+      </Row>
+      <br />
     </>
   );
 }
