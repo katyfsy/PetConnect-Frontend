847286dd69e792751a593f1110c1a345582a06bf
Benis Tambe <will.jenifer@yahoo.com>
2022-09-13 15:05:11 -0400

    pet list conditional display

--- app/src/Components/PetProfile/PetList.js
+++ app/src/Components/PetProfile/PetList.js
@@ -3,21 +3,33 @@ import PetCard from "./PetCard";
 
 function PetList() {
   const [petList, setPetList] = useState([]);
-
+  const [myPetList, setMyPetList] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
+  console.log(myPetList);
+  let user = localStorage.getItem("username");
+  console.log(localStorage.getItem("username"));
   useEffect(() => {
     fetch(
       "http://a920770adff35431fabb492dfb7a6d1c-1427688145.us-west-2.elb.amazonaws.com:8080/api/pets"
     )
       .then((res) => res.json())
       .then((data) => {
+        console.log("got data");
         setPetList(data.petsList);
+        if (user) {
+          fetch(`http://a920770adff35431fabb492dfb7a6d1c-1427688145.us-west-2.elb.amazonaws.com/:8080/api/pets?owner=${user}`)
+            .then((res) => res.json())
+            .then((Mydata) => {
+              setMyPetList(Mydata.petsList);
+            });
+        }
       });
   }, []);
 
   return (
     <>
-      <h1>List of Pets</h1>
-
+      <button onClick={() => setIsClicked(true)}>Show my pets</button>
+      <h1>List of All Pets</h1>
 
       <p
         style={{
@@ -29,16 +41,29 @@ function PetList() {
       >
         {/* {console.log(typeof petList)}
         {console.log(petList)} */}
-        {petList.map((petObj) => (
-          <PetCard
-            key={petObj.petId}
-            owner={petObj.owner}
-            name={petObj.name}
-            location={petObj.location}
-            type={petObj.type}
-            description={petObj.description}
-          />
-        ))}
+        {isClicked
+          ? myPetList.map((petObj) => (
+              <PetCard
+                id={petObj.petId}
+                key={petObj.petId}
+                owner={petObj.owner}
+                name={petObj.name}
+                location={petObj.location}
+                type={petObj.type}
+                description={petObj.description}
+              />
+            ))
+          : petList.map((petObj) => (
+              <PetCard
+                id={petObj.petId}
+                key={petObj.petId}
+                owner={petObj.owner}
+                name={petObj.name}
+                location={petObj.location}
+                type={petObj.type}
+                description={petObj.description}
+              />
+            ))}
       </p>
     </>
   );
