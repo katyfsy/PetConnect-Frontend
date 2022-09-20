e630595e562b055e65c67e7c3067a97a8c3d3e58
merwhite11 <meredith.white91@gmail.com>
2022-09-20 08:27:14 -0700

    slight change

--- app/src/Components/Search/AdvSearch.js
+++ app/src/Components/Search/AdvSearch.js
@@ -15,7 +15,7 @@ function AdvSearch({results, setResult, searchQuery, zipcode, radius, breed, set
   const [matches, setMatches] = useState(results)
 
   const [allBreeds, setAllBreeds] = useState(["Any"]);
-  const [allTypes, setAllTypes] = useState(["Any", "cat", "dog"]);
+  const [allTypes, setAllTypes] = useState(["Any", "cat", "dog", "other"]);
 
   const handleFilterClick = (e) => {
     var params = {zip: zipcode ? zipcode : null, type: type ? type : null, breed: breed ? breed : null, age: age ? age : null, sex: gender ? gender : null, radius: zipcode ? radius : null};
@@ -31,13 +31,14 @@ function AdvSearch({results, setResult, searchQuery, zipcode, radius, breed, set
   useEffect(() => {
     const searchArray = searchQuery.split(' ');
     if (searchArray.includes('dog')) setType('dog');
-    if (searchArray.includes('cat')) setType('cat');
+    else if (searchArray.includes('cat')) setType('cat');
   },[searchQuery])
 
   useEffect(() => {
     if (searchQuery === "" && zipcode === "") return;
     if (type === "Any") {
       var params = ""
+      //need to add param if type=other -- gets all non cat & dog breeds
     } else {
       params = "?type=" + type;
     }
@@ -50,7 +51,6 @@ function AdvSearch({results, setResult, searchQuery, zipcode, radius, breed, set
   useEffect(() => {
     if (searchQuery === "" && zipcode === "") return;
     var params = {zip: zipcode ? zipcode : null, type: type ? type : null, breed: breed ? breed : null, age: age ? age : null, sex: gender ? gender : null, radius: zipcode ? radius : null};
-    console.log(params);
     axios.get("http://localhost:8080/api/petSearch?search=*", {params})
     // axios.get("http://localhost:8080/api/petSearch", {params})
     .then((result) =>{
@@ -66,9 +66,7 @@ function AdvSearch({results, setResult, searchQuery, zipcode, radius, breed, set
 
     return (
       <>
-
       <div data-testid="adv-search" className="advSearchContainer">
-
       <div className="type filter">
           <h6 className="filterType">Type</h6>
           <select onChange={e=>setType(e.target.value)}>
