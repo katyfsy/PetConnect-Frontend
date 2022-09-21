973aa84cac601988726f24c68a7c99ec122591b8
Vincent Le <levin@oregonstate.edu>
2022-09-21 09:32:09 -0700

    moved pagination component into Directory

--- app/src/Pages/Directory.js
+++ app/src/Pages/Directory.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from "react";
 import Navigationbar from "../Components/Default/Navbar";
 import Header from "../Components/Default/Header";
 import Footer from "../Components/Default/Footer";
-import { Container, Row, Table } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Table,
+  DropdownButton,
+  Dropdown,
+  Col,
+} from "react-bootstrap";
 import User from "../Components/UserProfile/User";
 import DirectoryItem from "../Components/UserProfile/DirectoryItem";
+import ReactPaginate from "react-paginate";
 import {
   getBearerToken,
   getUser,
@@ -14,14 +22,51 @@ import axios from "axios";
 
 function Directory() {
   const [data, setData] = useState([]);
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
   const getData = async () => {
     const { data } = await axios.get(`${PSB_API_URL}/api/public/users/orgs`);
     setData(data.users);
   };
   useEffect(() => {
     getData();
-  }, []);
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(data.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(data.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, data]);
 
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.length;
+    setItemOffset(newOffset);
+  };
+
+  const rowItem = currentItems.map((element) => {
+    let blankCityState = "";
+    if (element.city === null && element.state === null) {
+      blankCityState = "";
+    } else {
+      blankCityState = element.city + ", " + element.state;
+    }
+    return (
+      <tr>
+        <td>
+          <a href={`/profile/${element.username}`}>{element.businessName} </a>
+        </td>
+        <td>
+          <a href={`/petlist/${element.username}`}>
+            {" "}
+            <i class="bi bi-house-heart hheart"></i>
+          </a>
+        </td>
+        <td>{blankCityState}</td>
+        <td>{element.phone}</td>
+        <td>{element.email}</td>
+      </tr>
+    );
+  });
 
   return (
     <>
@@ -32,6 +77,44 @@ function Directory() {
       <Container>
         <Row className="justify-content-center">
           <h1>Directory</h1>
+          <Row>
+            <Col>
+              <div className="pagination justify-content-left">
+                <ReactPaginate
+                  breakLabel="..."
+                  nextLabel="NEXT"
+                  onPageChange={handlePageClick}
+                  pageRangeDisplayed={5}
+                  pageCount={pageCount}
+                  previousLabel="PREV"
+                  renderOnZeroPageCount={null}
+                  breakClassName={"page-item"}
+                  breakLinkClassName={"page-link"}
+                  containerClassName={"pagination"}
+                  pageClassName={"page-item"}
+                  pageLinkClassName={"page-link"}
+                  previousClassName={"page-item"}
+                  previousLinkClassName={"page-link"}
+                  nextClassName={"page-item"}
+                  nextLinkClassName={"page-link"}
+                  activeClassName={"active"}
+                  className="pagination justify-content-left"
+                />
+              </div>
+            </Col>
+            <Col className="pagination justify-content-right">
+            <div>
+              <DropdownButton
+                id="dropdown-basic-button"
+                title="Dropdown button"
+              >
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
+              </div>
+            </Col>
+          </Row>
           <Table striped bordered hover>
             <thead>
               <tr>
@@ -42,7 +125,8 @@ function Directory() {
                 <th>Email</th>
               </tr>
             </thead>
-            <tbody ><DirectoryItem data={data}/></tbody>
+            <tbody>{rowItem}</tbody>
+            {/* <tbody ><DirectoryItem data={data}/></tbody> */}
           </Table>
         </Row>
         <Row>
