1c1bdca8aa3c1c7e2ac5f4bbc96ba7369338bbd3
Carlos Morales <65197354+cmoralespv@users.noreply.github.com>
2022-09-21 21:07:56 -0700

    merging

--- app/src/Components/Default/Navbar.js
+++ app/src/Components/Default/Navbar.js
@@ -22,50 +22,6 @@ function Navigationbar() {
   const [notification, setNotification] = useState(false);
   const location = useLocation();
 
-<<<<<<< HEAD
-  // if (username && location.pathname !== '/messages') {
-  //   useEffect(() => {
-  //     const id = setInterval(() =>
-  //       axios
-  //         .get(
-  //           `http://afea8400d7ecf47fcb153e7c3e44841d-1281436172.us-west-2.elb.amazonaws.com/messages/notifications/${username}`
-  //         )
-  //         // axios.get(`http://localhost:8080/messages/notifications/${username}`)
-  //         .then((response) => {
-  //           setNotification(response.data.length ? true : false);
-  //         })
-  //         .catch((err) => {
-  //           console.log(err);
-  //         }), 5000);
-  //     return () => clearInterval(id);
-  //   }, []);
-  // }
-
-  return (
-    <Navbar
-      expand="lg"
-      className="navBar"
-      style={{ paddingTop: 0, backgroundColor: "#8F9ED9", marginBottom: 10 }}
-    >
-      <Container>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {isLoggedIn() ? (
-              <Nav.Link href="/myprofile" style={{ color: "white" }}>
-                MyProfile
-              </Nav.Link>
-            ) : null}
-            <Nav.Link href="/pets" style={{ color: "white" }}>
-              Pets
-            </Nav.Link>
-            <Nav.Link href="/messages" style={{ color: "white" }}>
-              Messages
-            </Nav.Link>
-            {notification ? <AiFillMessage size={30} color={"red"} /> : null}
-            <Nav.Link href="/directory" style={{ color: "white" }}>
-              Directory
-            </Nav.Link>
-=======
   if (username && location.pathname !== '/messages') {
     useEffect(() => {
       const id = setInterval(() =>
@@ -94,7 +50,6 @@ function Navigationbar() {
             <Nav.Link href="/messages" style={{color: "white"}}>Messages</Nav.Link>
             {notification ? <AiFillMessage size={30} color={'red'} /> : null}
             <Nav.Link href="/directory" style={{color: "white"}}>Directory</Nav.Link>
->>>>>>> petprofile/main
           </Nav>
         </Navbar.Collapse>
       {/* </Container> */}
