c5d94db7bd3bfa555c25538b9b2100ab5067f469
Marilene Soares da Costa <lene626@gmail.com>
2022-09-21 18:20:13 -0600

    adds icons to quick search buttons - moves pics to public

--- app/src/Components/Search/QuickSearch.js
+++ app/src/Components/Search/QuickSearch.js
@@ -3,7 +3,6 @@ import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import './QuickSearch.css';
-import '../Default/HeaderPics/Dogs.png';
 
 
 function QuickSearch({setSearchQuery, setType, setResult}) {
@@ -33,23 +32,23 @@ function QuickSearch({setSearchQuery, setType, setResult}) {
         {/* <div className="col"> </div> */}
         <div className="col-md">
           <button id="allDogsBtn"
-          className=" quickBtn btn btn-outline-secondary"
+          className=" quickBtn btn"
           onClick={e => handleQuickSearchClick(e)}
           value="dog">
-             <img className="quickBtnImg" src="./../Default/HeaderPics/Dogs.png"/>
+             <img className="quickBtnImg" src="/Pics/Dogs.png"/>
             </button>
 
         </div>
         <div className="col-md">
-          <button id="allCatsBtn"  className=" quickBtn btn btn-outline-secondary" onClick={e => handleQuickSearchClick(e)} value="cat"><img className="quickBtnImg" src="https://images.pexels.com/photos/6638266/pexels-photo-6638266.jpeg?cs=srgb&dl=pexels-jan-kop%C5%99iva-6638266.jpg&fm=jpg" /></button>
+          <button id="allCatsBtn"  className=" quickBtn btn " onClick={e => handleQuickSearchClick(e)} value="cat"><img className="quickBtnImg" src="/Pics/Cats.png" /></button>
         </div>
 
         <div className="col-md">
-          <button id="otherPetsBtn" className=" quickBtn  btn btn-outline-secondary"  value="any"><img className="quickBtnImg" src="https://images.pexels.com/photos/7408291/pexels-photo-7408291.jpeg?cs=srgb&dl=pexels-alexas-fotos-7408291.jpg&fm=jpg"/></button>
+          <button id="otherPetsBtn" className=" quickBtn  btn "  value="any"><img className="quickBtnImg" src="/Pics/otherPets.png"/></button>
         </div>
 
         <div className="col-md">
-          <button id="orgsBtn"  className=" quickBtn btn btn-outline-secondary"  value="org"><img className="quickBtnImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Animal_Shelter_%288670%29_-_The_Noun_Project.svg/640px-Animal_Shelter_%288670%29_-_The_Noun_Project.svg.png"/></button>
+          <button id="orgsBtn"  className=" quickBtn btn "  value="org"><img className="quickBtnImg" src="/Pics/Shelters.png"/></button>
         </div>
         <div className="col"></div>
 
