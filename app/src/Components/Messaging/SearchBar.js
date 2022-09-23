1d872a973062f62b1e6b1d422485841924c0d6e7
Ginwoo Pak <ginwoopak@gmail.com>
2022-09-23 10:19:03 -0700

    searchbar mui

--- app/src/Components/Messaging/SearchBar.js
+++ app/src/Components/Messaging/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import CloseButton from 'react-bootstrap/CloseButton';
 import './css/SearchBar.css';
+import TextField from '@mui/material/TextField';
 
 const SearchBar = ({ privateChats, handleSearchInput, handleSearch, handleClear, searchRef }) => {
 
@@ -9,7 +10,13 @@ const SearchBar = ({ privateChats, handleSearchInput, handleSearch, handleClear,
     <form onSubmit={(event) => handleSearch(event)}>
       <div className='searchBar'>
         <img src='https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-1024.png' alt='Search icon' width='3%'/>
-        <input className='inputBar' placeholder={'Search messages...'} onChange={(event) => handleSearchInput(event)} value={searchRef}/>
+        <TextField
+            hiddenLabel
+            id='standard-basic'
+            label=''
+            variant='standard'
+            placeholder={'Search messages...'} onChange={(event) => handleSearchInput(event)} value={searchRef}
+          />
       </div>
     </form>
       {searchRef && <CloseButton onClick={(event) => handleClear(event)} aria-label='Clear search'/>}
