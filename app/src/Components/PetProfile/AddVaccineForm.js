235a1a85eee931c0abffeeecc2de6ce3b9746a6f
deleonenriqueta <enriqueta.correa@gmail.com>
2022-09-22 21:35:35 -0700

    edits to mirror addPet form styling

--- app/src/Components/PetProfile/AddVaccineForm.js
+++ app/src/Components/PetProfile/AddVaccineForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { FloatingLabel, Button, Form, Container } from "react-bootstrap";
+import { Formik, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 
 const AddVaccineForm = (props) => {
-
+  const handleOnChange = props.handleOnChange;
   const schema = Yup.object({
     name: Yup.string()
       .required("* Required")
@@ -52,6 +53,149 @@ const AddVaccineForm = (props) => {
         </div>
       </Form>
     </Formik>
+
+    // <Formik
+    //       validationSchema={schema}
+    //       // validateOnMount
+    //       // setSubmitting={false}
+    //       // onSubmit={console.log("VALUES: ", values)}
+    //       // validateOnChange={false}
+    //       // validateOnBlur={true}
+    //       // setTouched={false}
+    //       initialValues={{
+    //         vaccineName: "",
+    //         vaccineDate: "",
+    //         vaccineNotes: ""
+    //       }}
+    //       // validate
+    //       // errors={{
+    //       //   name: "",
+    //       //   city: "",
+    //       //   state: "",
+    //       //   zip: "",
+    //       //   type: "",
+    //       //   sex: "",
+    //       //   description: "",
+    //       //   // photos: "",
+    //       // }}
+    //     >
+    //       {({
+    //         handleSubmit,
+    //         handleChange,
+    //         handleBlur,
+    //         values,
+    //         touched,
+    //         isValid,
+    //         errors,
+    //       }) => (
+    //         <Form
+    //           className="addpet-form"
+    //           onSubmit={(e) => {
+    //             if (isValid === false) {
+    //               e.preventDefault();
+    //               e.stopPropagation();
+    //               setShowAlert(true);
+    //               setAlertTitle("Incomplete form");
+    //               setAlertText("Fill out required fields");
+    //               setAlertType("error");
+    //               setHandleOnExited(false);
+    //             } else {
+    //               handleSubmit(e);
+    //             }
+    //           }}
+    //         >
+    //           <Form.Group
+    //             className="mb-3 form-fields"
+    //             controlId="nameValidation"
+    //           >
+    //             <Form.Label>Vaccine Name</Form.Label>
+    //             <Form.Control
+    //               className="pet-name form-input"
+    //               type="text"
+    //               name="vaccineName"
+    //               value={values.vaccineName}
+    //               onChange={(e) => {
+    //                 handleChange(e);
+    //                 handleOnChange(e);
+    //               }}
+    //               placeholder="Pet's name"
+    //               isInvalid={errors.vaccineName}
+    //             />
+    //             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+    //             <Form.Control.Feedback className="form-error" type="invalid">
+    //               {errors.vaccineName}
+    //             </Form.Control.Feedback>
+    //           </Form.Group>
+
+    //           <Form.Group
+    //             className="mb-3 form-fields"
+    //             controlId="nameValidation"
+    //           >
+    //             <Form.Label>Date Administered</Form.Label>
+    //             <Form.Control
+    //               className="pet-name form-input"
+    //               type="text"
+    //               name="date"
+    //               value={values.vaccineDate}
+    //               onChange={(e) => {
+    //                 handleChange(e);
+    //                 handleOnChange(e);
+    //               }}
+    //               placeholder="Pet's name"
+    //               isInvalid={errors.vaccineDate}
+    //             />
+    //             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+    //             <Form.Control.Feedback className="form-error" type="invalid">
+    //               {errors.vaccineDate}
+    //             </Form.Control.Feedback>
+    //           </Form.Group>
+
+    //           <Form.Group
+    //             className="mb-3 form-fields"
+    //             controlId="nameValidation"
+    //           >
+    //             <Form.Label>Notes</Form.Label>
+    //             <Form.Control
+    //               className="pet-name form-input"
+    //               type="text"
+    //               name="notes"
+    //               value={values.vaccineNotes}
+    //               onChange={(e) => {
+    //                 handleChange(e);
+    //                 handleOnChange(e);
+    //               }}
+    //               placeholder="Pet's name"
+    //               isInvalid={errors.vaccineNotes}
+    //             />
+    //             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+    //             <Form.Control.Feedback className="form-error" type="invalid">
+    //               {errors.vaccineNotes}
+    //             </Form.Control.Feedback>
+    //           </Form.Group>
+
+    //           <div className="mb-3 buttons-form-container">
+    //             {/* <Form.Group className="mb-3">
+    //               <Button
+    //                 bsPrefix="cancel-pet-button"
+    //                 variant="secondary"
+    //                 type="submit"
+    //                 onClick={() => navigate(-1)}
+    //               >
+    //                 Cancel
+    //               </Button>
+    //             </Form.Group> */}
+
+    //             {/* <Form.Group className="mb-3">
+    //               {isClicked ? null : (
+    //                 <Button bsPrefix="add-pet-button" type="submit">
+    //                   Add Pet
+    //                 </Button>
+    //               )}
+    //             </Form.Group> */}
+    //           </div>
+    //         </Form>
+    //       )}
+    //     </Formik>
   );
 };
 
